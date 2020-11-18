de218290296a969b7b3045e3e791b1fc5561f5ea
Krzysztof Lezner <krzys3ek@gmail.com>
2020-11-18 11:46:26 +0100

    Add Cars listing

--- src/main/java/pl/kl/carworkshop/controller/CarController.java
+++ src/main/java/pl/kl/carworkshop/controller/CarController.java
@@ -2,6 +2,8 @@ package pl.kl.carworkshop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kl.carworkshop.service.CarService;
 
@@ -11,4 +13,10 @@ import pl.kl.carworkshop.service.CarService;
 public class CarController {
 
     private final CarService carService;
+
+    @GetMapping("")
+    public String getCars(Model model) {
+        model.addAttribute("listOfCars", carService.findAll());
+        return "car_list";
+    }
 }
--- src/main/java/pl/kl/carworkshop/service/CarService.java
+++ src/main/java/pl/kl/carworkshop/service/CarService.java
@@ -2,11 +2,18 @@ package pl.kl.carworkshop.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.kl.carworkshop.model.Car;
 import pl.kl.carworkshop.repository.CarRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CarService {
 
     private final CarRepository carRepository;
+
+    public List<Car> findAll() {
+        return carRepository.findAll();
+    }
 }
