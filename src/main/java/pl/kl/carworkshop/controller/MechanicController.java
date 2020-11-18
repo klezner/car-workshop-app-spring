91a46c32b8bcd310f2233f919b9951d2c7de828c
Krzysztof Lezner <krzys3ek@gmail.com>
2020-11-18 12:56:35 +0100

    Add Mechanics listing

--- src/main/java/pl/kl/carworkshop/controller/MechanicController.java
+++ src/main/java/pl/kl/carworkshop/controller/MechanicController.java
@@ -2,6 +2,8 @@ package pl.kl.carworkshop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kl.carworkshop.service.MechanicService;
 
@@ -11,4 +13,10 @@ import pl.kl.carworkshop.service.MechanicService;
 public class MechanicController {
 
     private final MechanicService mechanicService;
+
+    @GetMapping
+    public String getMechanics(Model model) {
+        model.addAttribute("listOfMechanics", mechanicService.findAll());
+        return "mechanic_list";
+    }
 }
--- src/main/java/pl/kl/carworkshop/service/MechanicService.java
+++ src/main/java/pl/kl/carworkshop/service/MechanicService.java
@@ -2,11 +2,18 @@ package pl.kl.carworkshop.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.kl.carworkshop.model.Mechanic;
 import pl.kl.carworkshop.repository.MechanicRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MechanicService {
 
     private final MechanicRepository mechanicRepository;
+
+    public List<Mechanic> findAll() {
+        return mechanicRepository.findAll();
+    }
 }
